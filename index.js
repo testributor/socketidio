e785ec4f7c10e3db8f9b0b81733f2e9e40fcea05
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-12-03 22:02:39 +0200

    Use socket.io rooms for resources

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
+var port = process.env.PORT || 9000
 var http = require('http').Server();
 var io = require('socket.io')(http);
-var redis = require("redis").createClient({host: '192.168.1.106'});
-var openSockets = [];
+var redis = require("redis").createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  db: process.env.REDIS_DB || 'katana_dev',
+  port: process.env.REDIS_PORT || 6379 });
 
-redis.on("message", function(channel, message) {
-  console.log(channel + ": " + message);
-  // TODO: Read the target subscribers from redis. Do not emit.
-  //for(i=0;i < openSockets.size; i++) { openSockets[i].  }
-  //"LiveUpdates:#{resource_key}:Subscribers", uid)
+// Returns a socket object matching the specified socket id.
+function socketFromId(socketId) {
+  return io.sockets.sockets.filter(
+    function(socket){ return socket.id == socketId; }
+  )[0];
+}
 
-
-  io.emit('LiveUpdate', message);
-});
-// This LivUpdate need not be the same with the one above (the one is Redis
-// the other is Socket.io/websockets.
-redis.subscribe("LiveUpdate");
-
+// TODO: This is only for debugging. No need to do anything on connection.
 io.on('connection', function(socket){
   console.log('Opened new socket: ' + socket.id);
-  openSockets.push(socket);
-  console.log(openSockets);
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+redisPublishCallbacks = {
+  'LiveUpdates': function(msg){
+    try {
+      parsedMsg = JSON.parse(msg);
+      room = parsedMsg["room"];
+      data = parsedMsg["data"];
+      io.to(room).emit(room, data);
+    } catch (err) { console.log(err) }
+  },
+  // http://socket.io/docs/rooms-and-namespaces/#
+  'RoomSubscriberAdd': function(msg) {
+    try {
+      parsedMsg = JSON.parse(msg);
+      socketId = parsedMsg["socket_id"];
+      if(socket = socketFromId(socketId)) {
+        socket.join(parsedMsg["room"]);
+      }
+    } catch (err) { console.log(err) }
+  }
+}
+
+// We subscribe to 2 different channels on Redis (Pub/Sub)
+// Whenever we receive a message from redis we call the relevant callback
+// for the channel.
+redis.on("message", function(channel, message) {
+  if(redisPublishCallbacks[channel]) {
+    redisPublishCallbacks[channel](message);
+  }
+});
+redis.subscribe("LiveUpdates"); // Where changes to be pushed to sockets are sent
+redis.subscribe("RoomSubscriberAdd"); // Where new subscribers for rooms are sent
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
